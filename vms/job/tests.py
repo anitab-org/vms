67c4b58703258665dc77d5bfcfe369f1d81d935a
vubo <vubogdanova@gmail.com>
2015-06-08 00:26:52 +0200

    New unit tests for job

--- vms/job/tests.py
+++ vms/job/tests.py
@@ -8,6 +8,44 @@ from volunteer.services import *
 
 class JobMethodTests(TestCase):
 
+    def test_delete_job(self):
+
+        e1 = Event(name = "Software Conference",
+                start_date = "2012-10-22",
+                end_date = "2012-10-25")
+
+        e1.save()
+
+        j1 = Job(name = "Software Developer",
+                start_date = "2012-10-22",
+                end_date = "2012-10-23",
+                description = "A software job",
+                event = e1)
+
+        j2 = Job(name = "Systems Administrator",
+                start_date = "2012-9-1",
+                end_date = "2012-10-26",
+                description = "A systems administrator job",
+                event = e1)
+
+        j3 = Job(name = "Project Manager",
+                start_date = "2012-1-2",
+                end_date = "2012-2-2",
+                description = "A management job",
+                event = e1)
+
+        j1.save()
+        j2.save()
+        j3.save()
+
+        #test typical cases
+        self.assertTrue(delete_job(j1.id))
+        self.assertTrue(delete_job(j2.id))
+        self.assertTrue(delete_job(j3.id))
+        self.assertFalse(delete_job(100))
+        self.assertFalse(delete_job(200))
+        self.assertFalse(delete_job(300))
+
     def test_get_job_by_id(self):
 
         e1 = Event(name = "Software Conference",
@@ -62,6 +100,45 @@ class JobMethodTests(TestCase):
         self.assertNotEqual(get_job_by_id(300), j1)
         self.assertNotEqual(get_job_by_id(300), j2)
         self.assertNotEqual(get_job_by_id(300), j3)
+        
+    def test_get_jobs_by_event_id(self):
+
+        e1 = Event(name = "Software Conference",
+                start_date = "2012-10-22",
+                end_date = "2012-10-25")
+
+        e1.save()
+
+        j1 = Job(name = "Software Developer",
+                start_date = "2012-10-22",
+                end_date = "2012-10-23",
+                description = "A software job",
+                event = e1)
+
+        j2 = Job(name = "Systems Administrator",
+                start_date = "2012-9-1",
+                end_date = "2012-10-26",
+                description = "A systems administrator job",
+                event = e1)
+
+        j3 = Job(name = "Project Manager",
+                start_date = "2012-1-2",
+                end_date = "2012-2-2",
+                description = "A management job",
+                event = e1)
+
+        j1.save()
+        j2.save()
+        j3.save()
+
+        #test typical case
+        job_list = get_jobs_by_event_id(e1.id)
+        self.assertIsNotNone(job_list)
+        self.assertNotEqual(job_list, False)
+        self.assertEqual(len(job_list), 3)
+        self.assertIn(j1, job_list)
+        self.assertIn(j2, job_list)
+        self.assertIn(j3, job_list)
 
     def test_get_jobs_ordered_by_title(self):
 
