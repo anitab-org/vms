294dec7b7a9ee83496ae77a8364dc504a9a6fff0
Ahmed Sabie <ahmed13579@gmail.com>
2015-12-21 14:15:17 -0500

    Remove empty events and jobs for volunteer

--- vms/job/services.py
+++ vms/job/services.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 
 from job.models import Job
-
+from shift.services import get_shifts_with_open_slots_for_volunteer
 
 def job_not_empty(job_id):
     """ Check if the job exists and is not empty """
@@ -52,3 +52,13 @@ def get_jobs_by_event_id(e_id):
 def get_jobs_ordered_by_title():
     job_list = Job.objects.all().order_by('name')
     return job_list
+
+
+def remove_empty_jobs_for_volunteer(job_list, volunteer_id):
+    """ Removes all jobs from a job list without shifts """
+    new_job_list = []
+    for job in job_list:
+        shift_list = get_shifts_with_open_slots_for_volunteer(job.id, volunteer_id)
+        if shift_list:
+            new_job_list.append(job)
+    return new_job_list
