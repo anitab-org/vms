87db79bb0fd33dd0bdc029f1202460d3f44a482f
vatsala <vatsala.swaroop@gmail.com>
2016-03-09 01:18:16 +0530

    Dealing with missing countries in the database

--- vms/registration/phone_validate.py
+++ vms/registration/phone_validate.py
@@ -2,9 +2,13 @@ from cities_light.models import Country
 import phonenumbers
 
 def validate_phone(my_country, my_phone):
-	entry = Country.objects.get(name__iexact=my_country)
-	country_code = entry.code2
-	print country_code
+	try:
+		entry = Country.objects.get(name__iexact=my_country)
+		country_code = entry.code2
+	except:
+		print ('No matching country in database')
+		return "missing"
+	print (country_code)
 	parsed_number = phonenumbers.parse( my_phone, country_code)
 	return (phonenumbers.is_valid_number(parsed_number) and phonenumbers.is_possible_number(parsed_number))
 	
