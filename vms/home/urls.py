65bc521d0ced0f55649a8a13b7854ce43defbf83
amruthasangeeth <amruthasangeeth@gmail.com>
2016-05-25 12:19:22 +0530

    Migrated home application to CBV
    Migrated home application from Function based view to Class based view

--- vms/home/urls.py
+++ vms/home/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 from home import views
+from django.views.generic import TemplateView
+
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', TemplateView.as_view(template_name='home/home.html'), name='index'),
 )
