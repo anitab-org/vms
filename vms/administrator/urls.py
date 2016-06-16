9a82ca4e98de09e7f361597f29f5f97a903a6c46
amruthasangeeth <amruthasangeeth@gmail.com>
2016-06-16 23:45:39 +0530

    Migrated administrator app to CBV
    Migrated the report generation function based view to CBV
    using FormView and ListView, updated requirements
    
    Added in the AdministratorLoginRequiredMixin to check admin
    privelages on report generation

--- vms/administrator/urls.py
+++ vms/administrator/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 from administrator import views
+from administrator.views import *
 
 urlpatterns = patterns('',
-    url(r'^report/$', views.report, name='report'),
+    url(r'^report/$', GenerateReportView.as_view(), name='report'),
     url(r'^settings/$', views.settings, name='settings'),
 )
