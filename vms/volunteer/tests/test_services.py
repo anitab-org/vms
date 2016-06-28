858b382bbe0ddf50b75d82dffbbb49641bae60f6
vatsala <vatsala.swaroop@gmail.com>
2016-06-28 13:51:17 +0530

    Added fixtures to volunteer tests

--- vms/volunteer/tests/test_services.py
+++ vms/volunteer/tests/test_services.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+import unittest
 
 from organization.models import Organization
 from volunteer.models import Volunteer
@@ -16,74 +16,43 @@ from volunteer.services import (delete_volunteer,
                                 create_volunteer_with_details)
 
 
-class VolunteerMethodTests(TestCase):
+class VolunteerMethodTests(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_delete_volunteer(self):
-
+    @classmethod
+    def setUpClass(cls):
         volunteer_1 = ['Yoshi',"Yoshi","Turtle","Mario Land","Nintendo Land","Nintendo State","Nintendo Nation","2374983247","yoshi@nintendo.com"]
         volunteer_2 = ['John',"John","Doe","7 Alpine Street","Maplegrove","Wyoming","USA","23454545","john@test.com"]
-        volunteer_3 = ['Ash',"Ash","Ketchum","Pallet Town","Kanto","Gameboy","Japan","23454545","ash@pikachu.com"]
+        volunteer_3 = ['Ash',"Ash","Doe","Pallet Town","Kanto","Gameboy","Japan","23454545","ash@pikachu.com"]
 
-        v1 = create_volunteer_with_details(volunteer_1)
-        v2 = create_volunteer_with_details(volunteer_2)
-        v3 = create_volunteer_with_details(volunteer_3)
-
-        self.assertTrue(delete_volunteer(v1.id))
-        self.assertTrue(delete_volunteer(v2.id))
-        self.assertTrue(delete_volunteer(v3.id))
-        self.assertFalse(delete_volunteer(100))
-        self.assertFalse(delete_volunteer(200))
-        self.assertFalse(delete_volunteer(300))
+        cls.v1 = create_volunteer_with_details(volunteer_1)
+        cls.v2 = create_volunteer_with_details(volunteer_2)
+        cls.v3 = create_volunteer_with_details(volunteer_3)
 
     def test_delete_volunteer_resume(self):
         """ Tests delete_volunteer_resume(volunteer_id) """
 
-        volunteer_1 = ['John',"John","Doe","7 Alpine Street","Maplegrove","Wyoming","USA","23454545","john@test.com"]
-        v1 = create_volunteer_with_details(volunteer_1)
-        v1.resume_file="MyResume.pdf"
-        v1.save()
+        self.v1.resume_file="MyResume.pdf"
+        self.v1.save()
 
-        self.assertTrue(delete_volunteer_resume(v1.id))
+        self.assertTrue(delete_volunteer_resume(self.v1.id))
         self.assertFalse(delete_volunteer(100))
 
     def test_get_all_volunteers(self):
         # Tests get_all_volunteers()
-
-        volunteer_1 = ['Yoshi',"Yoshi","Turtle","Mario Land","Nintendo Land","Nintendo State","Nintendo Nation","2374983247","yoshi@nintendo.com"]
-        volunteer_2 = ['John',"John","Doe","7 Alpine Street","Maplegrove","Wyoming","USA","23454545","john@test.com"]
-        volunteer_3 = ['Ash',"Ash","Ketchum","Pallet Town","Kanto","Gameboy","Japan","23454545","ash@pikachu.com"]
-
-        v1 = create_volunteer_with_details(volunteer_1)
-        v2 = create_volunteer_with_details(volunteer_2)
-        v3 = create_volunteer_with_details(volunteer_3)
 
         # test typical cases
         self.assertIsNotNone(get_all_volunteers())
 
     def test_get_volunteer_by_id(self):
 
-        volunteer_1 = ['Yoshi',"Yoshi","Turtle","Mario Land","Nintendo Land","Nintendo State","Nintendo Nation","2374983247","yoshi@nintendo.com"]
-        volunteer_2 = ['John',"John","Doe","7 Alpine Street","Maplegrove","Wyoming","USA","23454545","john@test.com"]
-        volunteer_3 = ['Ash',"Ash","Ketchum","Pallet Town","Kanto","Gameboy","Japan","23454545","ash@pikachu.com"]
-
-        v1 = create_volunteer_with_details(volunteer_1)
-        v2 = create_volunteer_with_details(volunteer_2)
-        v3 = create_volunteer_with_details(volunteer_3)
-
         # test typical cases
-        self.assertIsNotNone(get_volunteer_by_id(v1.id))
-        self.assertIsNotNone(get_volunteer_by_id(v2.id))
-        self.assertIsNotNone(get_volunteer_by_id(v3.id))
+        self.assertIsNotNone(get_volunteer_by_id(self.v1.id))
+        self.assertIsNotNone(get_volunteer_by_id(self.v2.id))
+        self.assertIsNotNone(get_volunteer_by_id(self.v3.id))
 
-        self.assertEqual(get_volunteer_by_id(v1.id), v1)
-        self.assertEqual(get_volunteer_by_id(v2.id), v2)
-        self.assertEqual(get_volunteer_by_id(v3.id), v3)
+        self.assertEqual(get_volunteer_by_id(self.v1.id), self.v1)
+        self.assertEqual(get_volunteer_by_id(self.v2.id), self.v2)
+        self.assertEqual(get_volunteer_by_id(self.v3.id), self.v3)
 
         # test non-existant cases
         self.assertIsNone(get_volunteer_by_id(100))
@@ -91,78 +60,59 @@ class VolunteerMethodTests(TestCase):
         self.assertIsNone(get_volunteer_by_id(300))
         self.assertIsNone(get_volunteer_by_id(400))
 
-        self.assertNotEqual(get_volunteer_by_id(100), v1)
-        self.assertNotEqual(get_volunteer_by_id(200), v1)
-        self.assertNotEqual(get_volunteer_by_id(300), v2)
-        self.assertNotEqual(get_volunteer_by_id(400), v2)
+        self.assertNotEqual(get_volunteer_by_id(100), self.v1)
+        self.assertNotEqual(get_volunteer_by_id(200), self.v1)
+        self.assertNotEqual(get_volunteer_by_id(300), self.v2)
+        self.assertNotEqual(get_volunteer_by_id(400), self.v2)
 
     def test_get_volunteer_resume_file_url(self):
 
-        volunteer_1 = ['Yoshi',"Yoshi","Turtle","Mario Land","Nintendo Land","Nintendo State","Nintendo Nation","2374983247","yoshi@nintendo.com"]
-        v1 = create_volunteer_with_details(volunteer_1)
-
-        v1.resume_file="MyResume.pdf"
-        v1.save()
+        self.v1.resume_file="MyResume.pdf"
+        self.v1.save()
 
         # test typical cases
-        self.assertIsNotNone(get_volunteer_resume_file_url(v1.id))
+        self.assertIsNotNone(get_volunteer_resume_file_url(self.v1.id))
         self.assertEqual(
-            get_volunteer_resume_file_url(v1.id),
-            v1.resume_file.url
+            get_volunteer_resume_file_url(self.v1.id),
+            self.v1.resume_file.url
             )
 
         # test non-existant cases
-        self.assertNotEqual(get_volunteer_resume_file_url(v1.id),
+        self.assertNotEqual(get_volunteer_resume_file_url(self.v1.id),
                             "resumes/DifferentResume.pdf"
                             )
-        self.assertNotEqual(get_volunteer_resume_file_url(v1.id),
+        self.assertNotEqual(get_volunteer_resume_file_url(self.v1.id),
                             "resumes/AnotherResume.pdf"
                             )
 
     def test_get_volunteers_ordered_by_first_name(self):
 
-        volunteer_1 = ['Yoshi',"Yoshi","Turtle","Mario Land","Nintendo Land","Nintendo State","Nintendo Nation","2374983247","yoshi@nintendo.com"]
-        volunteer_2 = ['John',"John","Doe","7 Alpine Street","Maplegrove","Wyoming","USA","23454545","john@test.com"]
-        volunteer_3 = ['Ash',"Ash","Ketchum","Pallet Town","Kanto","Gameboy","Japan","23454545","ash@pikachu.com"]
-
-        v1 = create_volunteer_with_details(volunteer_1)
-        v2 = create_volunteer_with_details(volunteer_2)
-        v3 = create_volunteer_with_details(volunteer_3)
-
         # test typical cases
         volunteer_list = get_volunteers_ordered_by_first_name()
         self.assertIsNotNone(volunteer_list)
-        self.assertIn(v1, volunteer_list)
-        self.assertIn(v2, volunteer_list)
-        self.assertIn(v3, volunteer_list)
+        self.assertIn(self.v1, volunteer_list)
+        self.assertIn(self.v2, volunteer_list)
+        self.assertIn(self.v3, volunteer_list)
         self.assertEqual(len(volunteer_list), 3)
 
         # test if in correct order
-        self.assertEqual(volunteer_list[0], v3)
-        self.assertEqual(volunteer_list[1], v2)
-        self.assertEqual(volunteer_list[2], v1)
+        self.assertEqual(volunteer_list[0], self.v3)
+        self.assertEqual(volunteer_list[1], self.v2)
+        self.assertEqual(volunteer_list[2], self.v1)
 
     def test_has_resume_file(self):
 
-        volunteer_1 = ['Yoshi',"Yoshi","Turtle","Mario Land","Nintendo Land","Nintendo State","Nintendo Nation","2374983247","yoshi@nintendo.com"]
-        volunteer_2 = ['John',"John","Doe","7 Alpine Street","Maplegrove","Wyoming","USA","23454545","john@test.com"]
-        volunteer_3 = ['Ash',"Ash","Ketchum","Pallet Town","Kanto","Gameboy","Japan","23454545","ash@pikachu.com"]
-
-        v1 = create_volunteer_with_details(volunteer_1)
-        v2 = create_volunteer_with_details(volunteer_2)
-        v3 = create_volunteer_with_details(volunteer_3)
-
-        v1.resume_file="MyResume.pdf"
-        v3.resume_file=""
-        v1.save()
-        v3.save()
+        self.v1.resume_file="MyResume.pdf"
+        self.v3.resume_file=""
+        self.v1.save()
+        self.v3.save()
 
         # test typical cases
-        self.assertTrue(has_resume_file(v1.id))
+        self.assertTrue(has_resume_file(self.v1.id))
 
         # test non-existant cases
-        self.assertFalse(has_resume_file(v2.id))
-        self.assertFalse(has_resume_file(v3.id))
+        self.assertFalse(has_resume_file(self.v2.id))
+        self.assertFalse(has_resume_file(self.v3.id))
 
     def test_search_volunteers(self):
 
@@ -172,42 +122,34 @@ class VolunteerMethodTests(TestCase):
         o1.save()
         o2.save()
 
-        volunteer_1 = ['Yoshi',"Yoshi","Doe","Mario Land","Nintendo Land","Nintendo State","Nintendo Nation","2374983247","yoshi@nintendo.com"]
-        volunteer_2 = ['John',"John","Doe","7 Alpine Street","Maplegrove","Wyoming","USA","23454545","john@test.com"]
-        volunteer_3 = ['Ash',"Ash","Ketchum","Pallet Town","Kanto","Gameboy","Japan","23454545","ash@pikachu.com"]
+        self.v1.organization=o1
+        self.v2.organization=o2
+        self.v3.unlisted_organization="Government of Canada"
 
-        v1 = create_volunteer_with_details(volunteer_1)
-        v2 = create_volunteer_with_details(volunteer_2)
-        v3 = create_volunteer_with_details(volunteer_3)
-
-        v1.organization=o1
-        v2.organization=o2
-        v3.unlisted_organization="Government of Canada"
-
-        v1.save()
-        v2.save()
-        v3.save()
+        self.v1.save()
+        self.v2.save()
+        self.v3.save()
 
         # if no search parameters are given,
         # it returns all volunteers
         search_list = search_volunteers("", "", "", "", "", "")
         self.assertNotEqual(search_list, False)
         self.assertEqual(len(search_list), 3)
-        self.assertIn(v1, search_list)
-        self.assertIn(v2, search_list)
-        self.assertIn(v3, search_list)
+        self.assertIn(self.v1, search_list)
+        self.assertIn(self.v2, search_list)
+        self.assertIn(self.v3, search_list)
 
         search_list = search_volunteers(None, None, None, None, None, None)
         self.assertNotEqual(search_list, False)
         self.assertEqual(len(search_list), 3)
-        self.assertIn(v1, search_list)
-        self.assertIn(v2, search_list)
-        self.assertIn(v3, search_list)
+        self.assertIn(self.v1, search_list)
+        self.assertIn(self.v2, search_list)
+        self.assertIn(self.v3, search_list)
 
         # test exact search
         search_list = search_volunteers(
                                         "Yoshi",
-                                        "Doe",
+                                        "Turtle",
                                         "Nintendo Land",
                                         "Nintendo State",
                                         "Nintendo Nation",
@@ -215,23 +157,23 @@ class VolunteerMethodTests(TestCase):
                                         )
         self.assertNotEqual(search_list, False)
         self.assertEqual(len(search_list), 1)
-        self.assertIn(v1, search_list)
-        self.assertNotIn(v2, search_list)
-        self.assertNotIn(v3, search_list)
+        self.assertIn(self.v1, search_list)
+        self.assertNotIn(self.v2, search_list)
+        self.assertNotIn(self.v3, search_list)
 
         # test partial search
         search_list = search_volunteers("Yoshi", None, None, None, None, None)
         self.assertNotEqual(search_list, False)
         self.assertEqual(len(search_list), 1)
-        self.assertIn(v1, search_list)
-        self.assertNotIn(v2, search_list)
-        self.assertNotIn(v3, search_list)
+        self.assertIn(self.v1, search_list)
+        self.assertNotIn(self.v2, search_list)
+        self.assertNotIn(self.v3, search_list)
 
         search_list = search_volunteers(None, "Doe", None, None, None, None)
         self.assertNotEqual(search_list, False)
         self.assertEqual(len(search_list), 2)
-        self.assertIn(v1, search_list)
-        self.assertIn(v2, search_list)
+        self.assertIn(self.v3, search_list)
+        self.assertIn(self.v2, search_list)
 
         # test no search matches
         search_list = search_volunteers(
@@ -243,6 +185,27 @@ class VolunteerMethodTests(TestCase):
                                         "Ubisoft"
                                         )
         self.assertEqual(len(search_list), 0)
-        self.assertNotIn(v1, search_list)
-        self.assertNotIn(v2, search_list)
-        self.assertNotIn(v3, search_list)
+        self.assertNotIn(self.v1, search_list)
+        self.assertNotIn(self.v2, search_list)
+        self.assertNotIn(self.v3, search_list)
+
+class DeleteVolunteerTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        volunteer_1 = ['Margaret',"Yoshi","Turtle","Mario Land","Nintendo Land","Nintendo State","Nintendo Nation","2374983247","yoshi1@nintendo.com"]
+        volunteer_2 = ['Miu',"John","Doe","7 Alpine Street","Maplegrove","Wyoming","USA","23454545","john1@test.com"]
+        volunteer_3 = ['Brock',"Ash","Ketchum","Pallet Town","Kanto","Gameboy","Japan","23454545","ash1@pikachu.com"]
+
+        cls.v1 = create_volunteer_with_details(volunteer_1)
+        cls.v2 = create_volunteer_with_details(volunteer_2)
+        cls.v3 = create_volunteer_with_details(volunteer_3)
+
+    def test_delete_volunteer(self):
+
+        self.assertTrue(delete_volunteer(self.v1.id))
+        self.assertTrue(delete_volunteer(self.v2.id))
+        self.assertTrue(delete_volunteer(self.v3.id))
+        self.assertFalse(delete_volunteer(100))
+        self.assertFalse(delete_volunteer(200))
+        self.assertFalse(delete_volunteer(300))
