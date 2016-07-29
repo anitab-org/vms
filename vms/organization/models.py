1f777235605f03ff881d0ef6027961ee8a8e9d9e
amruthasangeeth <amruthasangeeth@gmail.com>
2016-07-29 10:32:08 +0530

    Migrated Organization app to CBV

--- vms/organization/models.py
+++ vms/organization/models.py
@@ -2,8 +2,9 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 class Organization(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(
-        unique=True,            
+        unique=True,
         max_length=75,
         validators=[
             RegexValidator(
