daf353806b726176cdb87522bdc531775176f20c
vatsala <vatsala.swaroop@gmail.com>
2016-08-05 21:13:37 +0530

    Added locators for shift app

---
+++ vms/pom/locators/manageShiftPageLocators.py
@@ -0,0 +1,9 @@
+
+class ManageShiftPageLocators(object):
+
+    CANCEL_SHIFT_PATH = '//table//tbody//tr[1]//td[5]'
+    INFO_BOX = 'alert-info'
+    CANCELLATION_PANEL = 'panel-danger'
+    CANCELLATION_CONFIRMATION = 'btn-danger'
+    CANCELLATION_HEAD = 'panel-title'
+    SUBMIT_PATH = '//form[1]'
---
+++ vms/pom/locators/shiftDetailsPageLocators.py
@@ -0,0 +1,18 @@
+
+class ShiftDetailsPageLocators(object):
+	VOL_EMAIL = '//table[2]//tr//td[9]'
+	MAX_VOL = '//table[1]//tr//td[9]'
+	JOB = '//table[1]//tr//td[1]'
+	DATE = '//table[1]//tr//td[3]'
+	START_TIME = '//table[1]//tr//td[4]'
+	END_TIME = '//table[1]//tr//td[5]'
+	VIEW_DETAILS = '//table//tbody//tr[1]//td[7]'
+	
+	LOGGED_VOLUNTEER = '//table[3]//tr//td[1]'
+	LOGGED_START_TIME = '//table[3]//tr//td[4]'
+	LOGGED_END_TIME = '//table[3]//tr//td[5]'
+	LOGGED_VOLUNTEER_LIST = '//table[3]//tbody//tr'
+
+	REGISTERED_VOLUNTEER_NAME = '//table[2]//tr//td[1]'
+	REGISTERED_VOLUNTEER_LIST = '//table[2]//tbody//tr'
+	ALERT_BOX = 'alert-success'
