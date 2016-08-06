ea7b14880c86efa7e0fe15e04ccae8a8bbc30327
vatsala <vatsala.swaroop@gmail.com>
2016-08-06 09:11:59 +0530

    Added locators for job, event and home apps

---
+++ vms/pom/locators/homePageLocators.py
@@ -0,0 +1,19 @@
+
+class HomePageLocators(object):
+
+	ADMIN_REPORT_TEXT = 'Report'
+	VOLUNTEER_REPORT_TEXT = 'Report'
+	MANAGE_SHIFT_TEXT = 'Manage Volunteer Shifts'
+	UPCOMING_SHIFT_TEXT = 'Upcoming Shifts'
+	COMPLETED_SHIFT_TEXT = 'Completed Shifts'
+	LOGIN_TEXT = 'Log In'
+	LOGOUT_TEXT = 'Log Out'
+	ADMIN_EVENTS_TEXT = 'Events'
+	SHIFT_SIGN_UP_TEXT = 'Shift Sign Up'
+	VOLUNTEER_SEARCH_TEXT = 'Volunteer Search'
+	VOLUNTEER_PROFILE_TEXT = 'Profile'
+	CREATE_ADMIN_TEXT = 'Create Admin Account'
+
+	NO_ADMIN_RIGHT_HEAD = 'panel-heading'
+	NO_ADMIN_RIGHT_CONTENT = 'panel-body'
+	
---
+++ vms/pom/locators/jobDetailsPageLocators.py
@@ -0,0 +1,9 @@
+
+class JobDetailsPageLocators(object):
+
+	JOB_NAME = '//table[1]//tr//td[1]'
+	JOB_EVENT = '//table[1]//tr//td[2]'
+	JOB_START_DATE = '//table[1]//tr//td[3]'
+	JOB_END_DATE = '//table[1]//tr//td[4]'
+	VIEW_DETAILS = '//table[1]//tr//td[5]'
+	
