f68dcf67f149d613faa6d398d5a5b8de60f026d9
anjali-dhanuka <anjalidhanuka99@gmail.com>
2017-12-14 23:12:28 +0530

    password forgot feature implemented
    i18n and base.html loaded
    
    class based view used
    
    success_urls added
    
    email and subjects modified
    
    new line added
    
    forgot password done
    
    trailing whitespace error removed
    
    incorrect email validation added
    
    docstrings added
    
    docstring added according to pep287
    
    extra line addded
    
    whitespace removed
    
    errors sorted
    
    authentication forms modified
    
    unused imports removed

--- vms/authentication/urls.py
+++ vms/authentication/urls.py
@@ -1,10 +1,13 @@
 # Django
 from django.conf.urls import url
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import (LoginView, LogoutView, PasswordResetView, PasswordResetDoneView,
+                                       PasswordResetConfirmView, PasswordResetCompleteView)
 
 # local Django
 from authentication import views
 from authentication.views import anonymous_required
+from authentication.forms import EmailValidationOnForgotPassword
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -15,5 +18,16 @@ urlpatterns = [
     url(r'^logout/$',
         LogoutView.as_view(template_name='home/home.html'),
         name='logout_process'),
+     url(r'^password_reset/$',
+        PasswordResetView.as_view(success_url='done/',form_class=EmailValidationOnForgotPassword),
+        name='password_reset'),
+    url(r'^password_reset/done/$',
+        PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        PasswordResetConfirmView.as_view(success_url='/authentication/reset/complete/'), name='password_reset_confirm'),
+    url(r'^reset/complete/$',
+        PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+        name='password_reset_complete'),
 ]
 
