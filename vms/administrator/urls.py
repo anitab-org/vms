20586e0d1abb7f0cecc397031c2cb71e3f654087
sammy1997 <sombuddha2016@gmail.com>
2018-02-05 22:28:39 +0530

    Added url to view profile in base.html, and urls for edit and view profile page in urls.py

--- vms/administrator/urls.py
+++ vms/administrator/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import patterns, url
 
 # local Django
 from administrator import views
-from administrator.views import GenerateReportView
+from administrator.views import GenerateReportView, AdminUpdateView, ProfileView
 
 urlpatterns = patterns(
     '',
     url(r'^report/$', GenerateReportView.as_view(), name='report'),
     url(r'^settings/$', views.settings, name='settings'),
+    url(r'^edit/(?P<admin_id>\d+)$', AdminUpdateView.as_view(), name='edit'),
+    url(r'^profile/(?P<admin_id>\d+)$', ProfileView.as_view(), name='profile'),
+
 )
