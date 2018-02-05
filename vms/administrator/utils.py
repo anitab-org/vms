f3661693241e5116655e270d4ed68ee0bdf339d2
sammy1997 <sombuddha2016@gmail.com>
2018-02-05 22:26:50 +0530

    Added a util function to check admin_id, or raise errors otherwise

--- vms/administrator/utils.py
+++ vms/administrator/utils.py
@@ -4,6 +4,8 @@ from functools import wraps
 # Django
 from django.shortcuts import render
 
+from administrator.models import Administrator
+
 
 def admin_required(func):
     @wraps(func)
@@ -12,5 +14,26 @@ def admin_required(func):
         if not admin:
             return render(request, 'vms/no_admin_rights.html', status=403)
         return func(request, *args, **kwargs)
+
+    return wrapped_view
+
+
+def admin_id_check(func):
+    @wraps(func)
+    def wrapped_view(request, admin_id):
+        administrator = getattr(request.user, 'administrator',
+                                hasattr(request.user, 'administrator'))
+        if administrator is None:
+            return render(request, 'vms/no_admin_rights.html', status=403)
+        elif not administrator:
+            try:
+                admin = Administrator.objects.get(id=admin_id)
+                if not int(admin.id) == administrator.id:
+                    return render(
+                        request, 'vms/no_admin_rights.html', status=403)
+            except Administrator.DoesNotExist:
+                return render(
+                    request, 'vms/no_admin_rights.html', status=403)
+        return func(request, admin_id=admin_id)
 
     return wrapped_view
