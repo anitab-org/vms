e7035265d503cae8d51e2720e3cf7ef587478b9e
anjali-dhanuka <anjalidhanuka99@gmail.com>
2018-06-13 02:21:17 +0530

    description field added
    validations removed
    
    formatting implemented

--- vms/event/forms.py
+++ vms/event/forms.py
@@ -10,7 +10,7 @@ class EventForm(ModelForm):
     class Meta:
         model = Event
         fields = [
-            'name', 'start_date', 'end_date', 'country', 'state', 'city',
+            'name', 'description', 'start_date', 'end_date', 'country', 'state', 'city',
             'address', 'venue'
         ]
 
