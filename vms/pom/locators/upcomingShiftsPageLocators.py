be46508e750a1f0d08bbb0e69d5ef12e453cf0a6
anjali-dhanuka <anjalidhanuka99@gmail.com>
2018-07-21 11:12:18 +0530

    email message added for password test
    log hours upcoming shift tests
    
    docstring added

--- vms/pom/locators/upcomingShiftsPageLocators.py
+++ vms/pom/locators/upcomingShiftsPageLocators.py
@@ -4,7 +4,7 @@ class UpcomingShiftsPageLocators(object):
     SHIFT_DATE_PATH = '//table//tbody//tr[1]//td[2]'
     SHIFT_STIME_PATH = '//table//tbody//tr[1]//td[3]'
     SHIFT_ETIME_PATH = '//table//tbody//tr[1]//td[4]'
-    SHIFT_CANCEL_PATH = '//table//tbody//tr[1]//td[6]'
+    SHIFT_CANCEL_PATH = '//table//tbody//tr[1]//td[5]'
     LOG_SHIFT_HOURS_PATH = '//table//tbody//tr[1]//td[5]'
 
     INFO_BOX = 'alert-info'
