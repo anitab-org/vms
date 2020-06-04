101544730a5fc5ca8975be85bbe8d96494ff4933
Sankha Das <f20190029@pilani.bits-pilani.ac.in>
2020-06-04 23:57:56 +0530

    code: add staticfiles to urlpatterns in vms/urls.py (#968)
    Creates paths (or unique URLs) for static files such as PDFs or images.

--- vms/vms/urls.py
+++ vms/vms/urls.py
@@ -2,6 +2,9 @@
 from django.views.generic import RedirectView
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -23,3 +26,6 @@ urlpatterns = i18n_patterns(
     url(r'^volunteer/', include('volunteer.urls', namespace='volunteer')),
 )
 
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
